a5ae6eae52bf42f0d399666ae75573008ed99f20
Mark Thomas <markthethomas@gmail.com>
2015-12-17 22:01:48 -0800

    Add roll method

--- players/player.py
+++ players/player.py
@@ -1,8 +1,11 @@
 """
-
+Player class
 """
+import random
 
 class Player(object):
     """docstring for Player"""
-    def __init__(self):
-        pass
+    def __init__(self, balance=2000):
+        self.balance = balance
+    def roll(self):
+        return random.randint(0,6)
